055eda5c7237f2a85aba9e2d253752609e2bea58
Alan <terraplatform10@gmail.com>
2023-01-08 07:55:11 +0800

    Make

--- MakeAll.cmd
+++ MakeAll.cmd
@@ -18,7 +18,7 @@ SET OutFold=..\..\%OutFoldName%
 SET Module=Draw
 
 
-CALL %MethodsFilePath% :CreateModule "%Module%" "%Module%" "%OutFold%\Infra.lib %OutFold%\Infra.Form.Infra.lib"
+CALL %MethodsFilePath% :MakeModule "%Module%" "%Module%" "%OutFold%\Infra.lib %OutFold%\Infra.Form.Infra.lib"
 
 
 
@@ -27,7 +27,7 @@ CALL %MethodsFilePath% :CreateModule "%Module%" "%Module%" "%OutFold%\Infra.lib 
 SET Module=Draw.Exe
 
 
-CALL %MethodsFilePath% :CreateModule "%Module%" "%Module%" "%OutFold%\Draw.lib %OutFold%\Infra.lib %OutFold%\Infra.Form.Infra.lib" exe
+CALL %MethodsFilePath% :MakeModule "%Module%" "%Module%" "%OutFold%\Draw.lib %OutFold%\Infra.lib %OutFold%\Infra.Form.Infra.lib" exe
 
 
 
