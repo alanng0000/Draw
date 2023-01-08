b43bf46a5fe76488e47b126ac18737f107b9d08e
Alan <terraplatform10@gmail.com>
2023-01-08 10:37:32 +0800

    Draw

--- Draw/Public.h
+++ Draw/Public.h
@@ -24,30 +24,30 @@ ObjectApi(Draw, Draw)
 
 
 Draw_Api
-Int Draw_GetPixelList(Object this);
+Int Draw_Draw_GetPixelList(Object this);
 
 
 Draw_Api
-Bool Draw_SetPixelList(Object this, Int value);
-
-
-
-
-
-Draw_Api
-Object Draw_GetSize(Object this);
-
-
-Draw_Api
-Bool Draw_SetSize(Object this, Object value);
-
+Bool Draw_Draw_SetPixelList(Object this, Int value);
 
 
 
 
 
 Draw_Api
-Object Draw_Color(Object this, Int color, Object rect);
+Object Draw_Draw_GetSize(Object this);
+
+
+Draw_Api
+Bool Draw_Draw_SetSize(Object this, Object value);
+
+
+
+
+
+
+Draw_Api
+Object Draw_Draw_Color(Object this, Int color, Object rect);
 
 
 
