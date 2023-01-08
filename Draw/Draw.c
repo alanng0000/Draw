b3072ca0bd51876d3f06020838d92408c677e43d
Alan <terraplatform10@gmail.com>
2023-01-08 08:03:25 +0800

    Draw

--- Draw/Draw.c
+++ Draw/Draw.c
@@ -28,6 +28,62 @@ Bool Draw_Final(Object this)
 
 
 
+Int Draw_GetPixelList(Object this)
+{
+    Draw* m;
+
+    m = CastPointer(this);
+
+
+
+    Int32* p;
+    
+    p = m->PixelList;
+
+
+
+    Int o;
+
+    o = CastInt(p);
+
+
+    return o;
+}
+
+
+
+
+Bool Draw_SetPixelList(Object this, Int value)
+{
+    Draw* m;
+
+
+    m = CastPointer(this);
+
+
+
+
+    Int32* p;
+
+
+
+    p = CastPointer(value);
+    
+
+
+
+    m->PixelList = p;
+
+
+
+    return true;
+}
+
+
+
+
+
+
 
 Object Draw_Color(Object this, Int color, Object rect)
 {
