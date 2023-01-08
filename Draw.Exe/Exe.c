fe9af1ba3f59ca9f155929d7e7a024ab8ff9ff10
Alan <terraplatform10@gmail.com>
2023-01-08 10:42:58 +0800

    Draw

--- Draw.Exe/Exe.c
+++ Draw.Exe/Exe.c
@@ -8,7 +8,27 @@ int main(int argc, char* argv[])
     Infra_Form_Init();
 
 
+    Draw_Init();
 
+
+
+
+    Object draw;
+
+    draw = Draw_Draw_New();
+
+
+
+    Draw_Draw_Init(draw);
+
+
+
+    Draw_Draw_Final(draw);
+    
+
+
+
+    Draw_Draw_Delete(draw);
 
 
 
