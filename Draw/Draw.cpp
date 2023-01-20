229b9334123a7cefcbc50740d4401c8452ff1fc5
Alan <terraplatform10@gmail.com>
2023-01-20 16:53:07 +0800

    Draw

--- Draw/Draw.cpp
+++ Draw/Draw.cpp
@@ -5,5 +5,22 @@
 
 Object Draw_Draw_Create(Int hdc)
 {
+    void* p;
+
+    p = CastPointer(hdc);
+
+
+
+    Graphics* o;
+
+    o = new Graphics(p);
     
+
+
+    Object a;
+
+    a = CastInt(o);
+
+
+    return a;
 }
