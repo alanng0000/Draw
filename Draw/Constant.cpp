87c0c796344900b70462888350c803d62e5e4194
Alan <terraplatform10@gmail.com>
2023-01-22 05:28:58 +0800

    Draw

--- Draw/Constant.cpp
+++ Draw/Constant.cpp
@@ -3,3 +3,67 @@
 
 
 
+
+
+Object Draw_Constant_New()
+{
+    Global* m;
+
+    m = new Global();
+
+
+    Object o;
+
+    o = CastInt(m);
+
+
+    return o;
+}
+
+
+
+
+Bool Draw_Constant_Delete(Object o)
+{
+    void* p;
+
+    p = CastPointer(o);
+
+
+    delete p;
+
+
+    return true;
+}
+
+
+
+
+
+Bool Draw_Constant_Init(Object o)
+{
+    Constant* m;
+
+    m = Draw_Constant_Pointer(o);
+
+
+
+    return true;
+}
+
+
+
+
+
+
+
+
+Constant* Draw_Constant_Pointer(Object o)
+{
+    Constant* m;
+
+    m = (Constant*)o;
+
+
+    return m;
+}
