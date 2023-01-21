d6666240aef80dac8a987a9b6ab78e3f08e2fbc9
Alan <terraplatform10@gmail.com>
2023-01-21 13:47:55 +0800

    Cmd

--- MakeAll.cmd
+++ MakeAll.cmd
@@ -18,7 +18,7 @@ SET OutFold=..\..\%OutFoldName%
 SET Module=Draw
 
 
-CALL %MethodsFilePath% :MakeModule "%Module%" "%Module%" "%OutFold%\Infra.lib %OutFold%\Infra.Form.Infra.lib Gdiplus.lib" "1"
+CALL %MethodsFilePath% :MakeModule "%Module%" "%Module%" "%OutFold%\Infra.lib %OutFold%\Infra.Form.Infra.lib %OutFold%\Infra.Form.Windows.lib Gdiplus.lib" "1"
 
 
 
